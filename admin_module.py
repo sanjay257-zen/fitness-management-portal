20aab0fd27c09cf87d1a45ec5864874ce70fbba2
sanjay257-zen <senthilkumarsanjay257@gmail.com>
2026-04-25 22:05:45 +0800

    Create admin_module.py file with AdminModule class for fitness management portal.

---
+++ admin_module.py
@@ -0,0 +1,19 @@
+class AdminModule:
+    def __init__(self):
+        pass
+
+    def manage_planner(self):
+        """ Method to manage fitness planner. """
+        pass
+
+    def manage_trainer(self):
+        """ Method to manage trainers. """
+        pass
+
+    def view_statistics(self):
+        """ Method to view statistics. """
+        pass
+
+    def make_predictions(self):
+        """ Method to make predictions based on data. """
+        pass
